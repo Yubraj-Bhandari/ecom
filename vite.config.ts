181b989a78fb833e7c922915616d7a8d04541965
Yubraj-Bhandari <uvrajbhandari1@gmail.com>
2025-08-28 16:46:38 +0545

    changed vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,42 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+// import path from 'path'
+
+// export default defineConfig({
+//   base: "/ecom/", 
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//   ],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { copyFileSync } from 'fs'
 
 export default defineConfig({
-  base: "/ecom/", 
+  base: "/ecom/",
   plugins: [
     react(),
     tailwindcss(),
+    {
+      name: "copy-404",
+      closeBundle() {
+        copyFileSync(
+          path.resolve(__dirname, "dist/index.html"),
+          path.resolve(__dirname, "dist/404.html")
+        )
+      },
+    },
   ],
   resolve: {
     alias: {
